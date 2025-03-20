6db41f852e5c0bb997b047a0d6c4d1a0438ddbf1
Dmitriy Dolinov <dolinov91@gmail.com>
2025-03-20 17:57:57 +0300

    Функции: createGetter, invertObj, trimSymbols, uniq

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,21 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    return function (obj) {
+        const props = path.split('.')
 
+        let current = obj
+
+        for (let prop of props) {
+            if (current &&
+                typeof current === 'object' &&
+                current.hasOwnProperty(prop)) {
+                current = current[prop]
+            } else {
+                return undefined
+            }
+        }
+
+        return current
+    }
 }
