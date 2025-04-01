eee3dad96057e671ffaaf7fce51d65974ae23b58
Dmitriy Dolinov <dolinov91@gmail.com>
2025-04-01 22:06:20 +0300

    ColumnChart компонент

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,67 @@
 export default class ColumnChart {
-
-}
+    constructor(options = {}) {
+      this.data = options.data || []
+      this.label = options.label || ''
+      this.value = options.value || 0
+      this.link = options.link || ''
+      this.formatHeading = options.formatHeading || (value => value)
+      this.chartHeight = 50
+      this.element = this.createElement()
+    }
+  
+    createElement() {
+      const element = document.createElement('div')
+      element.className = `column-chart ${this.data.length === 0 ? 'column-chart_loading' : ''}`
+      element.innerHTML = `
+        <div class="column-chart__title">
+          Total ${this.label}
+          ${this.link ? `<a href="${this.link}" class="column-chart__link">View all</a>` : ''}
+        </div>
+        <div class="column-chart__container">
+          <div class="column-chart__header">${this.formatHeading(this.value)}</div>
+          <div class="column-chart__chart">
+            ${this.data.length === 0 ? 
+              '<img src="./charts-skeleton.svg" alt="Loading skeleton" />' : 
+              this.renderChartBars()}
+          </div>
+        </div>
+      `
+      return element
+    }
+  
+    renderChartBars() {
+      const maxValue = Math.max(...this.data)
+      const scale = this.chartHeight / maxValue
+      return this.data
+        .map(item => {
+          const scaledValue = Math.floor(item * scale)
+          const percent = ((item / maxValue) * 100).toFixed(0) + '%'
+          return `<div style="--value: ${scaledValue}" data-tooltip="${percent}"></div>`
+        })
+        .join('')
+    }
+  
+    update(newData) {
+      this.data = newData || []
+      const container = this.element.querySelector('.column-chart__container')
+      const hasData = this.data.length > 0
+      this.element.classList.toggle('column-chart_loading', !hasData)
+      container.innerHTML = `
+        <div class="column-chart__header">${this.formatHeading(this.value)}</div>
+        <div class="column-chart__chart">
+          ${hasData ? this.renderChartBars() : '<img src="./charts-skeleton.svg" alt="Loading skeleton" />'}
+        </div>
+      `
+    }
+  
+    remove() {
+      if (this.element) {
+        this.element.remove()
+      }
+    }
+  
+    destroy() {
+      this.remove()
+      this.element = null
+    }
+  }
